31207d5fb219df48cea28ed0179de42ea716d787
Nils Paulsson <nils.fj.paulsson@proton.me>
2026-06-10 02:08:08 +0200

    add validation annotations to start session request

--- src/main/java/io/github/nilsfjp/ideophonearena/dto/StartSessionRequest.java
+++ src/main/java/io/github/nilsfjp/ideophonearena/dto/StartSessionRequest.java
@@ -1,13 +1,16 @@
 package io.github.nilsfjp.ideophonearena.dto;
 
 import io.github.nilsfjp.ideophonearena.model.enums.ConditionName;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public class StartSessionRequest {
 
+    @NotNull
     @Positive
     private Integer difficultyLevel;
 
+    @NotNull
     private ConditionName conditionName;
 
     public Integer getDifficultyLevel() {
